19830b0829afc19bf9ffa58a9693597b0039bc0e
Andrew Tolstov <andrew.tolstov@gmail.com>
2020-05-21 13:52:50 -0400

    Initial Commit

---
+++ testTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTwo
+//
+//  Created by Andrey Tolstov on 5/21/20.
+//  Copyright © 2020 APNET HQ LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
